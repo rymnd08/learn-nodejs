043b7a7335b3168b374b21f5a3dc8127aca1821f
rymnd0800 <raymundregalado8@gmail.com>
2023-10-17 04:17:39 +0800

    read , write, delete file using fs

---
+++ global.js
@@ -0,0 +1,12 @@
+
+
+const people = ['kape', 'rymnd','rose']
+const ages = [100,22,21]
+// console.log(people)
+// console.log(__dirname)
+// console.log(__filename)
+
+module.exports  = {
+    people,
+    ages
+}

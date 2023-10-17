124b12adf50e1eae342fad401ac19ca025796347
rymnd0800 <raymundregalado8@gmail.com>
2023-10-17 20:08:12 +0800

    initial commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const _ = require('lodash')
-http.createServer( (req, res) =>{
-
-    res.end(JSON.stringify({kape: `${_.random(0, 100)}`}))
-}).listen(3000, ()=>{
-    console.log('server running on 3000')
-})

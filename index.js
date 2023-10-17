ffb1e49590f2e293bc76f790a5ba8b9997a70bb6
rymnd0800 <raymundregalado8@gmail.com>
2023-10-17 19:18:15 +0800

    add streams methods

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'application/json')
     // const person = {
     //     name: 'rymnd',
@@ -10,12 +10,16 @@ const server = http.createServer((req, res) =>{
     // res.write(JSON.stringify(person))
     // res.end()
 
-     res.setHeader('Content-Type', 'text/html')
-     fs.ReadStream()
+    res.setHeader('Content-Type', 'text/html')
 
+    const readStream = fs.createReadStream('./sample.txt', {encoding: 'utf-8'})
+    const writeStream = fs.createReadStream('./readSample.txt')
+    readStream.on('data', chunk =>{
+        writeStream()
+    })
 
 })
 
-server.listen(3000, 'localhost', ()=>{
+server.listen(3000, 'localhost', () => {
     console.log('server running on port 3000')
 })

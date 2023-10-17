b9d60e2310f7a72670a14ddbdd284427e7531efd
rymnd0800 <raymundregalado8@gmail.com>
2023-10-17 22:43:30 +0800

    query mysql data base

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const mysql = require('mysql')
+
+
+
 
 const app = express()
 console.log()
@@ -7,28 +11,62 @@ app.set('view engine', 'ejs')
 app.set('views', 'pages')
 
 //middlewares
-app.use(morgan('tiny'))
-app.use(express.static('assets'))
+// app.use(morgan('tiny'))
+// app.use(express.static('assets'))
 
 //server start
 PORT = 3000
 app.listen(PORT)
 
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "rms_narba"
+});
+
+let rabbits
+let users
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log('db connected')
+    con.query("SELECT * FROM `rabbit_profile_tbl`", function (err, result, fields) {
+        if (err) throw err;
+        console.log('get rabbits complete')
+        rabbits = result
+    });
+    con.query("SELECT * FROM users_tbl", function (err, result, fields) {
+        if (err) throw err;
+        console.log('get users complete')
+        users = result
+    });
+});
+
+
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+app.get('/rabbits', (req, res) => {
+    res.send(rabbits)
+})
+
 app.get('/', (req, res) => {
     const blogs = [
-        {title: 'title1', content: 'content1'},
-        {title: 'title2', content: 'content2'},
-        {title: 'title3', content: 'content3'},
+        { title: 'title1', content: 'content1' },
+        { title: 'title2', content: 'content2' },
+        { title: 'title3', content: 'content3' },
     ]
 
-    res.render('index', {name: 'RYMND J RGLD', blogs})
+    res.render('index', { name: 'RYMND J RGLD', blogs })
 })
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
-})
+// app.get('/about', (req, res) => {
+//     res.render('about')
+// })
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about')
+// })
 app.use((req, res) => {
     res.status(404).render('404')
 })

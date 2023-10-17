b44ae154b8c94518b7d0f09eb9e81ffe35ce6f6c
rymnd0800 <raymundregalado8@gmail.com>
2023-10-17 21:54:46 +0800

    finish

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
-
+console.log()
 app.set('view engine', 'ejs')
 app.set('views', 'pages')
 
+//middlewares
+app.use(morgan('tiny'))
+app.use(express.static('assets'))
 
+//server start
 PORT = 3000
 app.listen(PORT)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const blogs = [
+        {title: 'title1', content: 'content1'},
+        {title: 'title2', content: 'content2'},
+        {title: 'title3', content: 'content3'},
+    ]
+
+    res.render('index', {name: 'RYMND J RGLD', blogs})
 })
 app.get('/about', (req, res) => {
     res.render('about')

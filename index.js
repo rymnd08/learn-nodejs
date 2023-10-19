b90a2f179023014eb49799146e90e46eaf66a48e
rymnd0800 <raymundregalado8@gmail.com>
2023-10-19 08:52:32 +0800

    working multer

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const multer = require('multer')
 const router = express.Router()
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
-const upload = multer({dest: './uploads'})
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads'); // Specify the destination directory
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      let orignalName = file.originalname.split('.')
+      const ext = orignalName[orignalName.length-1]
+      const filename = `${uniqueSuffix}.${ext}`
+
+      cb(null, filename);
+    },
+  });
+const upload = multer({ storage : storage})
 
 
-router.route('/kape')
+router.route('/')
     .get((req, res) => {
-        res.send('kape')
+        res.send('home')
     })
-    .post((req, res)=>{
-        res.json({kape: 'mainit'})
+    .post(upload.single('image'), (req, res) => {
+
+        res.status(200).json({ response: 'kape' })
     })
 
+app.use(router)
 
 app.listen(3000)
